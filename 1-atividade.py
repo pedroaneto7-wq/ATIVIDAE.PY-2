fa3b1dbd08a2d4375d9ca39bbabc3933536a9f2e
pedroaneto7-wq <pedro.a.neto7@ba.estudante.senai.br>
2026-09-18 16:52:48 -0300

    Add files via upload

---
+++ 1-atividade.py
@@ -0,0 +1,11 @@
+import os
+os.system('cls')
+
+
+print('=tabuada=')
+
+numero = int(input('digite um numero '))
+
+for i in range(1, 11):
+
+    print(f'{numero} * {i} = {numero / i}')
---
+++ 2-atividade.py
@@ -0,0 +1,8 @@
+import os
+os.system('cls')
+
+numero = int(input('digite um numero'))
+
+for i in range(1, 11):
+    print(f'{numero} / {i} = {numero / i}')
+numero = int(input('digite um numero'))
